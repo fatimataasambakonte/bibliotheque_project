f8c0b76680626e0faa747675f34807cda9ac57dc
Fatimata Samba Konte <fatimataasambakonte@gmail.com>
2026-04-16 16:20:20 +0000

    Fix run.sh: binding sur $PORT pour Render

--- Devoir_Django/bibliotheque_project/run.sh
+++ Devoir_Django/bibliotheque_project/run.sh
@@ -4,5 +4,4 @@
 set -o errexit
 
 # Render injecte le port d'écoute via la variable $PORT
-# Remplacez 'monprojet' par le nom réel de votre projet Django
-gunicorn monprojet.wsgi:application --bind 0.0.0.0:$PORT --workers 2
+gunicorn bibliotheque_project.wsgi:application --bind 0.0.0.0:$PORT --workers 2
