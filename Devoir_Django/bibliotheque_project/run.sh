3f3ef3204fb9e14856b21c68f4f460a4b30a7340
Fatimata Samba Konte <fatimataasambakonte@gmail.com>
2026-04-17 11:14:52 +0000

    Fix run.sh: fichier créé proprement

--- Devoir_Django/bibliotheque_project/run.sh
+++ Devoir_Django/bibliotheque_project/run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-# run.sh — commande de démarrage du service Web
-
 set -o errexit
-
-# Render injecte le port d'écoute via la variable $PORT
 gunicorn bibliotheque_project.wsgi:application --bind 0.0.0.0:$PORT --workers 2
