3120b543761bc6d60315bb0baf9f3c8e189cdb4f
Fatimata Samba Konte <fatimataasambakonte@gmail.com>
2026-04-19 01:41:58 +0000

    Fix: recréer superuser avec bons droits

--- Devoir_Django/bibliotheque_project/build.sh
+++ Devoir_Django/bibliotheque_project/build.sh
@@ -12,13 +12,12 @@ python manage.py collectstatic --no-input
 # Appliquer les migrations
 python manage.py migrate
 
-# Créer le superuser automatiquement
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', '1234')
-    print('Superuser créé')
-else:
-    print('Superuser existe déjà')
+if User.objects.filter(username='admin').exists():
+    User.objects.filter(username='admin').delete()
+    print('Ancien admin supprimé')
+User.objects.create_superuser('oumy', 'admin@example.com', '1234')
+print('Superuser créé')
 "
