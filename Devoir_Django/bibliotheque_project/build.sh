e4b749d7537f98a703c908f267a17b68e23fa2b3
Fatimata Samba Konte <fatimataasambakonte@gmail.com>
2026-04-19 01:35:21 +0000

    Fix: créer superuser automatiquement

--- Devoir_Django/bibliotheque_project/build.sh
+++ Devoir_Django/bibliotheque_project/build.sh
@@ -11,3 +11,14 @@ python manage.py collectstatic --no-input
 
 # Appliquer les migrations
 python manage.py migrate
+
+# Créer le superuser automatiquement
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', '1234')
+    print('Superuser créé')
+else:
+    print('Superuser existe déjà')
+"
